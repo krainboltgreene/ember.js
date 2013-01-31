dccd4400190c3d377cbff7f787807e29d2d10068
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-01-31 09:42:38 -0800

    Adding the latest distribution

--- packages/ember-handlebars/lib/controls.js
+++
@@ -1,5 +0,0 @@
-require("ember-handlebars/controls/checkbox");
-require("ember-handlebars/controls/text_field");
-require("ember-handlebars/controls/button");
-require("ember-handlebars/controls/text_area");
-require("ember-handlebars/controls/select");
